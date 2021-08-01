930ce12f2ddb756faf471dedb6f1c5d0aeb52c00
Switchback <aidartemirgaliev@gmail.com>
2021-08-02 01:42:39 +0600

    remove unused ui

--- code/ui/SandboxHud.cs
+++ code/ui/SandboxHud.cs
@@ -19,7 +19,5 @@ public partial class SandboxHud : HudEntity<RootPanel>
 		RootPanel.AddChild<Scoreboard<ScoreboardEntry>>();
 		RootPanel.AddChild<Health>();
 		RootPanel.AddChild<InventoryBar>();
-		RootPanel.AddChild<CurrentTool>();
-		RootPanel.AddChild<SpawnMenu>();
 	}
 }
