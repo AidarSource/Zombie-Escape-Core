c5acb3adcb5b0aa1b05bf3fd0cc304174424bc11
Switchback <aidartemirgaliev@gmail.com>
2021-10-17 01:20:44 +0600

    upd

---
+++ code/ui/ZeScoreboard.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox.UI;
+
+
+public partial class ZeScoreboard : Panel
+{
+	public static ZeScoreboard Current;
+
+	public ZeScoreboard()
+	{
+		Current = this;
+		StyleSheet.Load( "/ui/DamageIndicator.scss" );
+	}
+}
