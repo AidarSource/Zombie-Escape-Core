c44820d6f02229c02d06f9c8897bd266c320bf44
Switchback <aidartemirgaliev@gmail.com>
2021-10-22 00:20:00 +0600

    restyle of roundresult ui

--- code/ui/RoundResult.cs
+++ code/ui/RoundResult.cs
@@ -1,11 +1,19 @@
 ﻿using Sandbox.UI;
+using Sandbox.UI.Construct;
 
-public partial class RoundResult : Label
+public partial class RoundResult : Panel
 {
+	public Label Label;
+	public RoundResult()
+	{
+		Label = Add.Label( "Result", "result" );
+	}
+
 	public override void Tick()
 	{
 		SetClass( "hidden", ((ZeCore)ZeCore.Current).RoundCounter == 0 );
-		SetText( ((ZeCore)ZeCore.Current).RoundResultText );
+		//SetText( ((ZeCore)ZeCore.Current).RoundResultText );
+		Label.Text = ((ZeCore)ZeCore.Current).RoundResultText;
 		SetProperty( "color", "red" );
 	}
 }
